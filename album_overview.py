6205a86492ae7e9b66100b42b422ff94b1c82c3e
Robin Hawkins <robinthawkins@gmail.com>
2020-05-28 21:43:23 -0500

    v0.8

---
+++ album_overview.py
@@ -0,0 +1,100 @@
+# Return list of all ablum tracks w/attributes based on the album id
+import pandas as pd 
+import spotipy 
+import json
+import os
+import pprint
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+import mpld3
+from scipy import stats
+from pandas.io.json import json_normalize
+import ipywidgets as widgets
+from ipywidgets import interact, interact_manual
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import lyricsgenius
+import re
+import plotly.graph_objects as go
+from song_overview import clean_lyrics
+from song_overview import sentiment_analyzer_scores
+import rh_config
+from rh_config import token
+from rh_config import sp
+from rh_config import genius
+
+
+# genius = lyricsgenius.Genius("4I9B6Ee7dz4ZielbofNgpRCnnnkpfAbQSReqWo3K0Ge7UDG2JBy6rKc-B5Rbl6zf")
+# sp = spotipy.Spotify() 
+# from spotipy.oauth2 import SpotifyClientCredentials 
+# cid ="5a1378624b714bd49ac1f55eb755f579" 
+# secret = "7bb688c810ad47a68dae9a80aa384e75" 
+# client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) 
+# sp.trace=False 
+analyser = SentimentIntensityAnalyzer()
+
+
+def analyze_album(album_id):
+        tracks = []
+        track_ids = []
+        results = sp.album_tracks(album_id)
+        tracks.extend(results['items'])
+        while results['next']:
+            results = sp.next(results)
+            tracks.extend(results['items'])
+        for track in tracks:
+            track_ids.append(track['id'])
+        track_str = ','.join(map("'{0}'".format, track_ids)) 
+        analysis_df = json_normalize(sp.audio_features(tracks=track_ids))
+        tracks_df = json_normalize(sp.album_tracks(album_id)["items"])
+        df = analysis_df.merge(tracks_df, on='id', how='inner')
+        album_name = sp.album(album_id)["name"]
+        album_name = clean_lyrics(album_name)
+        release_date = sp.album(album_id)["release_date"]
+
+        regex = '\[.*?\]'
+        regex2 = '\-.*'
+
+        titles = df["name"]
+        artist = json_normalize(sp.album_tracks(album_id)["items"][0]["artists"])["name"][0]
+        
+        keys = {0:'C',1:'C#',2:'D',3:'D#',4:'E',5:'F',6:'F#',7:'G',8:'G#',9:'A',10:'A#',11:'B'}
+        
+        df["key"] = df['key'].map(keys, na_action='ignore')
+        
+        mode = {0:'Minor',1:'Major'}
+        
+        df["mode"] = df['mode'].map(mode, na_action='ignore')
+
+        df["duration"] = (df["duration_ms_x"]/(1000*60))%60
+    
+        
+        df['track'] = df['track_number']
+        df = df.loc[df["disc_number"]==1]
+        df = df.set_index('track_number')
+        df = df[["name", "energy", "valence", "danceability", "loudness", "tempo", "key", "mode","time_signature","duration","id","track"]]
+        df = df.rename(columns={"valence": "mus_valence"})
+        
+        energy_z = abs(stats.zscore(df["energy"]))
+        valence_z = abs(stats.zscore(df["mus_valence"]))   
+        dance_z = abs(stats.zscore(df["danceability"]))
+        duration_z = abs(stats.zscore(df["duration"])) 
+        loudness_z = abs(stats.zscore(df["loudness"])) 
+        df["uniqueness"] = (energy_z + valence_z + dance_z + duration_z + loudness_z) / 5
+
+        # print("Breakdown:")
+        # print("")
+        # print("Representative: " + df.loc[df['uniqueness'].idxmin()]["name"])
+        # print("Unique: " + df.loc[df['uniqueness'].idxmax()]["name"])
+        # print("Energetic: " + df.loc[df['energy'].idxmax()]["name"])
+        # print("Slowest: " + df.loc[df['energy'].idxmin()]["name"])
+        # print("Positive: " + df.loc[df['mood'].idxmax()]["name"])
+        # print("Negative: " + df.loc[df['mood'].idxmin()]["name"])
+        # print("Loudest: " + df.loc[df['loudness'].idxmax()]["name"])
+        # print("Quietest: " + df.loc[df['loudness'].idxmin()]["name"])
+        
+        df = df.to_dict('records')
+        df = json.dumps(df)
+        return df
+    
---
+++ app.py
@@ -0,0 +1,47 @@
+import song_overview
+import album_overview
+from flask import Flask, redirect, jsonify, render_template, request
+from flask_pymongo import PyMongo
+import json
+import logging
+
+from bson import ObjectId
+from bson import json_util
+from flask.json import JSONEncoder
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+app.json_encoder = JSONEncoder()
+
+#################################################
+# Flask Routes
+#################################################
+
+
+@app.route("/",methods=['GET', 'POST'])
+def welcome():
+    return render_template("index.html")
+
+@app.route("/<song_id>")
+def song_data(song_id):
+    song_dict = song_overview.get_song_features(song_id)
+    album_new_data = album_overview.analyze_album(song_overview.get_album_id(song_id))
+    return render_template("view_track.html", song_dict=song_dict, album_new_data = album_new_data)
+
+@app.route("/album/<album_id>")
+def album_data(album_id):
+    album_new_data = album_overview.analyze_album(album_id)
+    return album_new_data
+
+@app.route('/search_result', methods=['POST'])
+def search_result():
+    selected_song = song_overview.search_song_id(request.form['track-search'])
+    song_dict = song_overview.get_song_features(selected_song)
+    album_new_data = album_overview.analyze_album(song_overview.get_album_id(selected_song))
+    return render_template("view_track.html", song_dict=song_dict, album_new_data = album_new_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
